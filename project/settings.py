c4ede6ba7040f7cc9577d142890f324d6ba64f93
Aniket1298 <tekina1298@gmail.com>
2020-08-26 12:26:03 +0530

    Changes made

--- project/settings.py
+++ project/settings.py
@@ -129,13 +129,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 MEDIA_URL='/media/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_URL='login'
 LOGIN_REDIRECT_URL='home'
-STATICFILES_DIRS = [
-    ('node_modules', os.path.join(BASE_DIR, 'node_modules')),
-]
